8747cd0b5d72a7ad17e0886a6d13c3a8789169d7
Syahrul Arifuddin <sayamanusia2000@gmail.com>
2020-08-06 00:28:07 +0700

    write file

--- solution.js
+++ solution.js
@@ -5,6 +5,7 @@ const pMap = require('p-map')
 const cheerio = require('cheerio')
 const got = require('got')
 const { resolve } = require('url')
+const fs = require('fs')
 
 const baseUrl = 'https://www.cermati.com'
 
@@ -16,46 +17,55 @@ exports.main = async () => {
             const $article = $(article)
             const $link = $article.find('a')
             const url = resolve(baseUrl, $link.attr('href'))
-            const title = $article.find('h3').text().trim()
-            const postingDate = $article.find('.item-description .item-publish-date').children().eq(1).text().trim()
             return {
-                url,
-                title,
-                postingDate
+                url
             }
         } catch (err) {
             console.log('parse error', err)
         }
     }).filter(Boolean)
-    return (await pMap(articles, processDetailPage, {
+    // use 'p-map' to set a practical limit on parallelism whether it be throttling network bandwidth or compute resources.
+    // drop-in replacement for Promise.all(…), which doesn’t support limiting parallelism.
+    return (await pMap(articles, processArticlePage, {
         concurrency: 3
     })).filter(Boolean)
 }
 
-// url ^
-// title ^ 
-// postingDate ^
-// author
-// relatedArticles -> url, title
-
-async function processDetailPage(article){
-    console.warn('processing article', article.url)
+async function processArticlePage(article){
     try {
         const {body} = await got(article.url)
         const $ = cheerio.load(body)
-        const authorName = $('.author-name').text().trim()
+        const author = $('.author-name').text().trim()
+        const postingDate = $('.post-date > span').text().trim()
+        const title = $('h1.post-title').text().trim()
+        const relatedArticles = $('.panel-items-list li').slice(0,5).get().map(relatedArticle => {
+            const $relatedArticle = $(relatedArticle)
+            const $link = $relatedArticle.find('a')
+            const url = resolve(baseUrl, $link.attr('href'))
+            const title = $relatedArticle.find('h5').text().trim()
+            return {
+                url,
+                title
+            }
+        })
         return {
-            authorName
+            url: article.url,
+            title,
+            author,
+            postingDate,
+            relatedArticles
         }
     } catch (error) {
-        
+        console.error(error.message)
     }
 }
 
 if (!module.parent) {
     exports.main()
         .then((articles) => {
-            console.log(JSON.stringify(articles, null, 2))
+            const json = JSON.stringify({articles}, null, 2)
+            console.log(json)
+            fs.writeFileSync('solution.json', json)
             process.exit(0)
         })
         .catch((err) => {
